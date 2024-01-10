601d378cf643c15521d7ba27a34b67e1dc0ddf49
Soel Attala <soelattala7@gmail.com>
2024-01-10 19:20:30 -0300

    Challenge 3.1 foreach and matrix

---
+++ 3.1-Challenge/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+
+    Ejercicio: Completar una actividad de desafío para instrucciones de iteración y selección anidadas
+
+El equipo ha encontrado un patrón. Los pedidos que comienzan por la letra "B" presentan un fraude con una
+frecuencia 25 veces superior a la normal. Deberá escribir otro código que genere el identificador de pedido 
+para los nuevos pedidos que empiecen por la letra "B". El equipo antifraude lo usará para investigar más en 
+profundidad.
+
+*/
+
+string[] pedidos = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+
+foreach (string pedido in pedidos)
+{
+    if (pedido.StartsWith("B"))
+    {
+        Console.WriteLine($"Los pedidos con B son:{pedido} ");
+    }
+}
