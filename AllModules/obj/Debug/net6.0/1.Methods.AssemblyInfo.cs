ff38c18daf85d2ba1636e6891fb82804f3f06529
Soel Attala <soelattala7@gmail.com>
2024-01-24 14:29:50 -0300

    Adding StudentGrading App

--- AllModules/obj/Debug/net6.0/1.Methods.AssemblyInfo.cs
+++ AllModules/obj/Debug/net6.0/1.Methods.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1.Methods")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c582fa6f1993e6aa996e877cab799c58cf5f8b29")]
 [assembly: System.Reflection.AssemblyProductAttribute("1.Methods")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1.Methods")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
