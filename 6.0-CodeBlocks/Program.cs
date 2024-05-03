87ecf02d39749fc3ddffc0ed9e301e3b23c5c550
Soel Attala <soelattala7@gmail.com>
2024-05-03 15:58:43 -0300

    adding new modules

---
+++ 6.0-CodeBlocks/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+INTRODUCCION:
+Las instrucciones de selección e iteración usan bloques de código para agrupar las líneas de código que se deben
+ejecutar, omitir o recorrer en iteración. Pero no es su única finalidad. También se pueden usar bloques de código
+para controlar o limitar la accesibilidad a las variables. El "ámbito" de la variable hace referencia a la parte
+de una aplicación donde se puede acceder a una variable. Comprender cómo un bloque de código afecta al ámbito de
+una variable es una parte importante de la programación.
+
+Supongamos que trabaja en una aplicación grande que usa instrucciones de iteración y selección anidadas para procesar
+datos de matriz. La aplicación usa variables para ayudar a realizar tareas comunes en toda la aplicación.
+Algunas variables tienen la misma finalidad en diferentes partes de la aplicación, y ha intentado reutilizar 
+los nombres de variable. A medida que crece la aplicación, empieza a ver resultados inesperados en los cálculos y
+errores que notifican que una variable no se ha inicializado o no existe. Debe mejorar el enfoque que usa para declarar
+y acceder a las variables, y debe mejorar la comprensión del ámbito de las variables.
+
+En este módulo, declarará variables para su uso dentro y fuera de los límites de los bloques de código.
+Eliminará los bloques de código en determinadas situaciones para que el código sea más legible. Aprenderá cómo afectan
+los bloques de código a la accesibilidad y la visibilidad de las variables.
+
+Al final de este módulo, podrá usar los bloques de código con mayor confianza y comprenderá cómo influyen
+en la visibilidad y la accesibilidad del código.
+
+
+Ejercicio: Los bloques de código y el ámbito de las variables
+Un bloque de código es una o varias instrucciones de C# que definen una ruta de acceso de ejecución. Las instrucciones 
+fuera del bloque de código influyen en cómo, en qué casos y con qué frecuencia se ejecuta ese bloque de código en tiempo 
+de ejecución. Los límites de un bloque de código normalmente se definen mediante corchetes, {}.
+
+Además de su efecto en la ruta de acceso de ejecución, los bloques de código también pueden afectar al ámbito de las variables.
+Los ejemplos de código que examine durante este ejercicio le ayudarán a comprender la relación entre los bloques de código y el
+ámbito de variable.
+
+Los bloques de código afectan al ámbito de las declaraciones de variable
+El ámbito de variable hace referencia a la visibilidad de una variable para el código restante de la aplicación. Una variable de
+ámbito local solo es accesible dentro del bloque de código en el que está definida. Si intenta acceder a la variable fuera del
+bloque de código, recibirá un error del compilador.
+
+El resto de esta unidad explora la relación entre los bloques de código y el ámbito de variable.
+
+*/
+
+bool flag = true;
+if (flag)
+{
+    int value2 = 10;
+    Console.WriteLine($"Inside the code block: {value2}");
+}
+
+
+
+//Intentaremos ultilizar la variable value desde fuera del bloque
+//Console.WriteLine($"Outside the code block: {value}");
+//como podemos observar obtenemos un error.
+
+int value;
+value = 10;
+
+//ahora que ya declaramos value si podemos acceder desde fuera del bloque
+Console.WriteLine($"Outside the code block: {value}");
+
+//ahora tendremos o que actualizar el valor dentro del bloque a value = 10 en lugar de
+// int value = 10, o podemos cambiar el nombre de value a value 2.
+
---
+++ 7.0-Switch-Case-Conditionals/Program.cs
@@ -0,0 +1,220 @@
+﻿
+/*
+
+                                Ejercicio: Implementacion de una shitch.
+
+Una instrucción switch es una instrucción de selección de C# que proporciona una alternativa a una construcción de bifurcación if-elseif-else. La instrucción switch proporciona ventajas sobre una construcción if-elseif-else al evaluar un valor único en una lista de valores coincidentes conocidos.
+
+Considere el caso siguiente:
+
+Está trabajando en una aplicación relacionada con la nutrición alimentaria. Una sección del código se ocupa 
+de las frutas.El código incluye una variable denominada fruit que se usa para contener el nombre de diferentes 
+tipos de fruta.
+Dispone de una lista de 20 frutas en las que se centra la aplicación.
+Quiere bifurcar el código en función del valor asignado a fruit.
+En este escenario, puede usar una instrucción switch para crear una rama independiente para cada tipo de fruta.
+
+
+
+                                ¿Cómo funciona una instrucción switch?
+
+La instrucción switch elige una sección de código que se va a ejecutar en una lista de posibles secciones switch.
+La sección switch seleccionada se elige en función de una coincidencia de patrón con la expresión de coincidencia
+de la instrucción.
+
+Un ejemplo sería:
+switch (fruit)
+{
+    case "apple":
+        Console.WriteLine($"App will display information for apple.");
+        break;
+
+    case "banana":
+        Console.WriteLine($"App will display information for banana.");
+        break;
+
+    case "cherry":
+        Console.WriteLine($"App will display information for cherry.");
+        break;
+}
+
+
+
+La expresión match (que también se puede denominar expresión switch) es el valor que sigue a la palabra clave
+switch, en este caso (fruit). Cada sección switch se define mediante un patrón de caso. Los patrones de caso se
+construyen mediante la palabra clave case seguida de un valor. El primer patrón de caso de este ejemplo es case
+"apple":. Los patrones de caso son expresiones booleanas que se evalúan como true o false. Cada sección switch
+incluye un pequeño número de líneas de código que se ejecutarán si el patrón de caso es una coincidencia para
+la expresión match. En este ejemplo, si a fruit se le asigna un valor de "apple", el primer patrón de caso se 
+evaluará como true y se ejecutará esa sección switch.
+
+Una instrucción switch debe incluir al menos una sección switch, pero normalmente contendrá 
+tres o más secciones switch.
+
+Switch se aprovecha mejor cuando:
+
+Dispone de un valor único (variable o expresión) que debe emparejar con muchos valores posibles.
+Para cualquier coincidencia determinada, debe ejecutar un par de líneas de código como máximo.
+
+
+*/
+
+int employeeLevel = 200;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+    case 100:
+        title = "Junior Associate";
+        break;
+    case 200:
+        title = "Senior Associate";
+        break;
+    case 300:
+        title = "Manager";
+        break;
+    case 400:
+        title = "Senior Manager";
+        break;
+    default:
+        title = "Associate";
+        break;
+}
+
+Console.WriteLine($"{employeeName}, {title}");
+
+
+/*
+                                Desafío de conversión de instrucciones a switch.
+En este desafío reescribiremos una construcción if-elseif-else como instrucción switch. Este desafío nos ayudará 
+a ver las ventajas y los inconvenientes de usar la instrucción switch en comparación con una construcción 
+if-elseif-else.
+
+            Desafío de código: Reescritura de if-elseif-else mediante una instrucción switch.
+Comenzaremos con el código que usa una construcción if-elseif-else para evaluar los componentes de una SKU de 
+producto. 
+
+La SKU (referencia de almacén) tiene el formato de tres valores codificados:
+<product #>-<2-letter color code>-<size code>.
+
+Por ejemplo, un valor de SKU de 01-MN-L corresponde a (sweat shirt)-(maroon)-(large) y el código genera una
+descripción que aparece como "Product: Large Maroon Sweat shirt".
+
+El desafío es convertir el código de la instrucción if en una instrucción switch que logre el mismo resultado 
+que el código inicial.
+
+*/
+
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
+
+string[] product = sku.Split('-');
+
+string type = "";
+string color = "";
+string size = "";
+
+if (product[0] == "01")
+{
+    type = "Sweat shirt";
+}
+else if (product[0] == "02")
+{
+    type = "T-Shirt";
+}
+else if (product[0] == "03")
+{
+    type = "Sweat pants";
+}
+else
+{
+    type = "Other";
+}
+
+if (product[1] == "BL")
+{
+    color = "Black";
+}
+else if (product[1] == "MN")
+{
+    color = "Maroon";
+}
+else
+{
+    color = "White";
+}
+
+if (product[2] == "S")
+{
+    size = "Small";
+}
+else if (product[2] == "M")
+{
+    size = "Medium";
+}
+else if (product[2] == "L")
+{
+    size = "Large";
+}
+else
+{
+    size = "One Size Fits All";
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
+
+
+
+//Resolución:
+
+
+switch (product[0])
+{
+    case "01":
+        type = "Sweat shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat pants";
+        break;
+    default:
+        type = "Other";
+        break;
+}
+
+switch (product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+}
+
+switch (product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
+
