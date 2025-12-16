9d9f846865356c918c8baf7af784fe34834be684
Soel Antonio Attala <soelattala@outlook.com>
2025-12-16 20:38:44 -0300

    creating new guide project

---
+++ CsharpProjects/AuxiliarIndexOF/Program.cs
@@ -0,0 +1,6 @@
+﻿string message = "hello there!";
+
+int first_h = message.IndexOf('h');
+int last_h = message.LastIndexOf('h');
+
+Console.WriteLine($"For the message: '{message}', the first 'h' is at position {first_h} and the last 'h' is at position {last_h}.");
